0f289ff6e2b5b0cbc736ace6d07002307e9c7e09
2FAST4U <yassinegt92@gmail.com>
2026-05-14 09:37:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const STORAGE_KEY = 'sci_maryas_manager_v1';
+const AUTO_MONTHLY_KEY = 'sci_maryas_auto_monthly_generation_v1';
+
+const defaultData = {
+  settings: {
+    companyName: 'SCI MARYAS',
+    managerName: '',
+    email: '',
+    phone: '',
+    address: '35 RUE DES CAILLOUX',
+    city: 'CLICHY',
+    postalCode: '92110',
+    siret: '80420228100049',
+    iban: '',
+    currency: 'EUR',
+    legalNote: 'Merci de conserver ce document.',
+  },
+  clients: [],
+  documents: [],
+};
+
+// Collez ensuite le reste de votre app.js ici.
